71163737df054a397141f2f283e581384acc1a74
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-13 18:35:55 +0100

    retoques

--- Ejercicio5_RAIL_FENCE_CHYPER.py
+++ Ejercicio5_RAIL_FENCE_CHYPER.py
@@ -3,17 +3,19 @@ class rail_fence_chyper:
         self.cuadricula = cuadricula
     def codificar(mensaje, clave):
         fila, columna = 0, 0
-        
+        i = 1
         tabla1 = [["." for cols in range(len(mensaje))] for rows in range(clave)]
         while columna < len(mensaje):
-            if fila + 1 < 0 or fila + 1 >= clave:
+            if fila + i < 0 or fila + i >= clave:
                 i = i*-1
             tabla1[fila][columna]= mensaje[columna]
             fila = fila + i
             columna = columna + 1
-            
+
         Encripcion = ""
         for j in tabla1:
             Encripcion = Encripcion + "".join(j)
             print(j)
         return Encripcion
+
+    
