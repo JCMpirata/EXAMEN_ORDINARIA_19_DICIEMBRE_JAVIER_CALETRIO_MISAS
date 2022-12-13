7df033daf5024ff69fb07148880ff7bd8840a26d
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-13 20:28:14 +0100

    mas fibonacci

--- Ejercicio4_FIBONACCI.py
+++ Ejercicio4_FIBONACCI.py
@@ -1,9 +1,18 @@
-def Fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n < 0:
-        print("Error: n debe ser mayor o igual a 0")
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+
+class cifra:
+    
+        def __init__(self, numero):
+            self.numero = numero
+    
+        def fibonacci(self):
+            if self.numero == 0:
+                return 0
+            elif self.numero == 1:
+                return 1
+            elif self.numero < 0:
+                print("Error: n debe ser mayor o igual a 0")
+            else:
+                return self.fibonacci(self.numero-1) + self.fibonacci(self.numero-2)
+
+        
+    
