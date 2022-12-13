4b37f6720e1683b533e6246f7a381a3f90f6562c
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-13 20:28:55 +0100

    terminado

--- Ejercicio4_FIBONACCI.py
+++ Ejercicio4_FIBONACCI.py
@@ -14,5 +14,18 @@ class cifra:
             else:
                 return self.fibonacci(self.numero-1) + self.fibonacci(self.numero-2)
 
-        
+        def Fibonacci(self):
+            lista = []
+            for i in range(0, self.numero):
+                if self.numero <= 2000000:
+                    lista.append(self.fibonacci(i))
+                else:
+                    print("Error: n debe ser menor o igual a 2000000")
+
+
+
+if __name__ == '__main__':
+    numero = int(input("Ingrese un número: "))
+    fibonacci = cifra(numero)
+    print(fibonacci.Fibonacci())
     
